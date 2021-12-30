1872e8d35d6dbc3dbde7382650f2cecbda063b81
searles9 <donovansearles@gmail.com>
2021-12-30 11:46:55 -0500

    added functionality so it sends all the jobs in 1 excel document

--- job_scraper/main.py
+++ job_scraper/main.py
@@ -19,7 +19,7 @@ EMAIL_INFO = {
 
 # ----------------------------------
 
-def filter_security_roles(all_jobs):
+def filter_security(all_jobs):
     good_terms = ["Security","Cyber"]
     bad_terms = ["Senior","Lead","Azure","Principal","Release","Sr","Software",
                  "Manager", "Financal","Finance","Payroll","HR","Marketing","SEO",
@@ -27,7 +27,7 @@ def filter_security_roles(all_jobs):
                  "Litigation","Payable","Vehicle","Arbitrator","UX","Recruiter"]
     return utils.filter_jobs_by_title(all_jobs, good_terms, bad_terms)
 
-def general_filter_roles(all_jobs):
+def filter_general(all_jobs):
     good_terms = ["Security","Solutions","Cloud","Cyber","Reliability","Architect","Systems","System","Infrastructure"]
     bad_terms = ["Senior","Lead","Azure","Principal","Release","Sr","Software",
                  "Manager", "Financal","Finance","Payroll","HR","Marketing","SEO",
@@ -37,34 +37,38 @@ def general_filter_roles(all_jobs):
 
 if __name__ == "__main__":
     # Scrape Jobs
-    if DEBUG: # Grabs less
-        test_url = "https://jobs.coxenterprises.com/job-search-results/?keyword=security&business_unit=Cox%20Automotive&category[]=Information%20Technology&level=Individual%20Contributor&employment_type=Full-time&location=Norcross%2C%20GA%2C%20US&latitude=33.906&longitude=-84.184&radius=25"
-        all_jobs = scrape_jobs.get_jobs(test_url)
-    else: # Gathers all jobs
-        # Get all jobs close to Atlanta GA (25 mile radius / full-time / individual contributor)
-        site_url = "https://jobs.coxenterprises.com/job-search-results/?level=Individual%20Contributor&employment_type=Full-time&location=Atlanta%2C%20GA%2C%20USA&latitude=33.7489954&longitude=-84.3879824&radius=25&nationwide=US&statewide=GA"
-        all_jobs = scrape_jobs.get_jobs(site_url)
- 
-    # Filter for security roles
-    security_jobs = filter_security_roles(all_jobs)
-    # General filter to filter out bad roles
-    general_filtered_roles = general_filter_roles(all_jobs)
+    if DEBUG:
+        # Local - pulls less results for debugging
+        test_local_url = "https://jobs.coxenterprises.com/job-search-results/?keyword=security&business_unit=Cox%20Automotive&category[]=Information%20Technology&level=Individual%20Contributor&employment_type=Full-time&location=Norcross%2C%20GA%2C%20US&latitude=33.906&longitude=-84.184&radius=25"
+        all_local_jobs = scrape_jobs.get_jobs(test_local_url)
+        # Remote Jobs
+        test_remote_url = "https://jobs.coxenterprises.com/job-search-results/?keyword=security&level=Individual%20Contributor&employment_type=Full-time&location=Norcross%2C%20GA%2C%20US&latitude=33.906&longitude=-84.184&radius=25&locationType=Nationwide"
+        all_remote_jobs = scrape_jobs.get_jobs(test_remote_url)
+    else:
+        # Local (25 mile radius around Atlanta - full-time, individual contributor)
+        local_site_url = "https://jobs.coxenterprises.com/job-search-results/?level=Individual%20Contributor&employment_type=Full-time&location=Atlanta%2C%20GA%2C%20USA&latitude=33.7489954&longitude=-84.3879824&radius=25&nationwide=US&statewide=GA"
+        all_local_jobs = scrape_jobs.get_jobs(local_site_url)
+        # Remote Jobs (full-time, individual contributor))
+        remote_url = "https://jobs.coxenterprises.com/job-search-results/?level=Individual%20Contributor&employment_type=Full-time&location=Norcross%2C%20GA%2C%20US&latitude=33.906&longitude=-84.184&radius=25&locationType=Nationwide"
+        all_remote_jobs = scrape_jobs.get_jobs(remote_url)
+    all_jobs = all_local_jobs + all_remote_jobs
+
+    # Filter all jobs to create new data sets
+    security_roles = filter_security(all_jobs)
+    general_filter_roles = filter_general(all_jobs)
 
     if SEND_EMAIL:
-        # Add file names to create
-        all_jobs_file = "All_Jobs.xlsx"
-        security_jobs_file = "Security_Jobs.xlsx"
-        filtered_jobs_file = "Filtered_Jobs.xlsx"
-        EMAIL_INFO["FILES"].extend([all_jobs_file, security_jobs_file, filtered_jobs_file])
-        # Create files
-        utils.jobs_to_excel(all_jobs,all_jobs_file)
-        utils.jobs_to_excel(security_jobs,security_jobs_file)
-        utils.jobs_to_excel(general_filtered_roles,filtered_jobs_file)
-        # Send email
+        file = "Jobs.xlsx"
+        EMAIL_INFO["FILES"].extend([file])
+        # Generate main file with all jobs
+        utils.jobs_to_excel(all_jobs,file)
+        # Add new sheets in the existing excel file for each filter
+        utils.add_excel_sheet(security_roles,file,"Security-Filter")
+        utils.add_excel_sheet(security_roles,file,"General-Filter")
+        # Send the email
         send_gmail.send_gmail_email(EMAIL_INFO)
-        # Delete files from the local machine 
+        # Delete the place files from the local machine 
         utils.delete_files(EMAIL_INFO["FILES"])
     else:
+        print("ALL JOBS:")
         print(utils.print_jobs_bullet_points(all_jobs))
-
-
